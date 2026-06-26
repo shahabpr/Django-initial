eaac38a0a0c3a7307a750c26751c71806a527c24
Shahab <cheebuy333@gmail.com>
2026-06-26 23:27:31 +0330

    first commit

--- days/views.py
+++ days/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponseNotFound, HttpResponseRedirect , Http404
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.urls import reverse
 
 # from django.template.loader import render_to_string
@@ -20,7 +21,10 @@ day_list = list(days.keys())
 
 def dynamic_days(request, day):
     day_data = days.get(day)
-    # -> dares 14 -> render template + template filters + template tags
+    if day_data is None:
+        raise Http404()
+        # page_not_found = render_to_string('404.html')
+        # return HttpResponseNotFound(page_not_found)
 
     context = {
         'day': day,
@@ -30,17 +34,15 @@ def dynamic_days(request, day):
 
 
 def index(request):
-    # -> dars 15 -> template tags initially
     context = {
         'days': day_list
     }
     return render(request, "days/days.html", context)
 
 
-
 def dynamic_days_by_number(request, day_number):
     if day_number > len(day_list):
-        return HttpResponseNotFound('the day number not exist in day numbers')
+        return HttpResponseNotFound('the day number not exist')
     day = day_list[day_number - 1]
     url_path = reverse('dynamic_days', args=[day])
     return HttpResponseRedirect(url_path)
